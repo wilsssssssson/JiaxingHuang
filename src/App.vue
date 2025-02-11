f71e65fcce0abc1b8e9d6abdd0786e423834eea5
wilson <1327003704@qq.com>
2025-02-11 17:19:19 +0800

    一些问题

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+console.log(import.meta.env.VITE_BASE_URL)</script>
 
 <template>
 	<router-view></router-view>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
 import { createHtmlPlugin } from 'vite-plugin-html';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  
+
   build: {
     outDir: "docs"
   },
